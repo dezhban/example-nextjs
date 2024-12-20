219d9f62151f9f8ba281248966cd5fdabb11594f
Hamed Shirzadpour <shirzadpour@outlook.com>
2024-12-20 11:11:36 +0330

    fix: typo error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,17 @@ import Link from "next/link";
 export default async function Home() {
   const session = await authClient.getSession();
 
+  const isLoggedIn = session?.user !== undefined;
+
   return (
     <div>
       <h1>App Dashboard</h1>
       <h3>
-        {session?.isLoggedIn
+        {isLoggedIn
           ? `Wellcome ${session.user?.name}`
           : "User is not logged in"}
       </h3>
-      {session?.isLoggedIn ? (
+      {isLoggedIn ? (
         <Link href="/auth/logout">Logout</Link>
       ) : (
         <Link href="/auth/login">Login</Link>
